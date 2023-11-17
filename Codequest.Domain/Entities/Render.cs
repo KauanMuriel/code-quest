bae7f73840b951b15d1c9765946dbb93b432a59a
Daniel Bonam <daniiel.bonam@gmail.com>
2023-11-17 15:15:11 -0300

    Render partially fixed
    Mapa está sendo apresentado invertido, correção deve ser algo simples.

--- Codequest.Domain/Entities/Render.cs
+++ Codequest.Domain/Entities/Render.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,40 +21,37 @@ namespace CodeQuest
 
             Console.WriteLine("Player Lives: " + _player.Lives);
 
+            int lineCount = 0;
             foreach (var tile in Map.MapDesign)
             {
-                int countY = 0;
-                while ((tile.Y + countY++) != Map.YSize)
+                if (tile.Y != lineCount)
                 {
-                    int countX = 0;
-                    // COLUNA 1
-                    while ((tile.X + countX++) != Map.XSize)
-                    {
-                        // LINHA
-                        switch (tile.Type)
-                        {
-                            case TileMapType.Obstacle:
-                                Console.Write(" # ");
-                                break;
-                            case TileMapType.Heal:
-                                Console.Write("|+|");
-                                break;
-                            case TileMapType.Danger:
-                                Console.Write("|-|");
-                                break;
-                            default:
-                                if (tile.X == _player.Position[0] && tile.Y == _player.Position[1])
-                                {
-                                    Console.Write("[0]");
-                                }
-                                else
-                                {
-                                    Console.Write("   ");
-                                }
-                                break;
-                        }
-                    }
+                    lineCount++;
                     Console.WriteLine();
+                }
+                switch (tile.Type)
+                {
+                    case TileMapType.Obstacle:
+                        Console.Write(" # ");
+                        break;
+                    case TileMapType.Danger:
+                        Console.Write("|-|");
+                        break;
+                    case TileMapType.Heal:
+                        Console.Write("|+|");
+                        break;
+                    case TileMapType.SpawnPoint:
+                        Console.Write(" ? ");
+                        break;
+                    default:
+                        if (tile.X == _player.Position[0] && tile.Y == _player.Position[1])
+                        {
+                            Console.Write("[0]");
+                        } else
+                        {
+                            Console.Write("   ");
+                        }
+                        break;
                 }
             }
         }
